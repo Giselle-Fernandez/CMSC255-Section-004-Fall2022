c16294f24b664ab06b6d325f44078a74d25c8ac3
Giselle-Fernandez <112010268+Giselle-Fernandez@users.noreply.github.com>
2022-08-24 13:32:58 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/******************************************************************************* 
+ * Hello.java
+******************************************************************************** 
+* Giselle Fernandez
+* 08/24/2022
+* CMSC255 004
+********************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Giselle Fernandez");
+		System.out.println("Hello.java");
+		System.out.println("Being able to compile and run a Java program");
+		System.out.println("08/24/2022");
+		System.out.println("CMSC255 004");
+		System.out.println();
+
+	}
+}
